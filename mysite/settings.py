4b2c6cf95affdf3c78ee787b69f59b43730f6e26
Malin <larsson.malin87@googlemail.com>
2018-06-02 11:36:08 +0200

    added dot to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '--=sp$*as30jd$dmli)va33tz^a)(!q$(vh%r8=)1voju(o*57'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
